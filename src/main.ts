18479c1797bba4a618adfea481d27e5e9e4b4993
Imbugcolor <viethd123456@gmail.com>
2024-04-03 11:02:50 +0700

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import { NextFunction, Request, Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    next();
-  });
+  app.enableCors();
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const configService = app.get<ConfigService>(ConfigService);
